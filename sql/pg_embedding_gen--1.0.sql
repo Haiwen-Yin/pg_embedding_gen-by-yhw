39661453e85a32f350110bb4ad566215900bd97c
Haiwen-Yin <52781124+Haiwen-Yin@users.noreply.github.com>
2026-05-11 22:43:35 +0800

    0.2.0 commit

--- sql/pg_embedding_gen--1.0.sql
+++
@@ -1,7 +0,0 @@
--- pg_embedding_gen Extension Control File
-
--- Extension name and version metadata
-COMMENT ON EXTENSION pg_embedding_gen IS 'BGE-M3 Embedding Generation for PostgreSQL 18';
-
--- Version history
--- v1.0.0 - Initial release with BGE-M3 via HTTP API integration
